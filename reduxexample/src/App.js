31fab2a96e23db825b40fa27a1aa20d0c8947b99
Philip Walsh <philipwalsh.ds@gmail.com>
2020-12-29 14:31:58 -0500

    first commit

---
+++ reduxexample/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Posts from './components/Posts.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
